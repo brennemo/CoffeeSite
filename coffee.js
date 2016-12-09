c7b97ccf80e326888f626158288dc9886a14da27
Morgan <brennemo@oregonstate.edu>
2016-12-09 14:15:16 -0500

    Added simple test

--- coffee.js
+++ coffee.js
@@ -1,23 +1,36 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res) {
-	
-	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-	switch(path) {
-		case '':
-			res.writeHead(200, { 'Content-Type': 'text/plain'});
-			res.end('Homepage');
-			break;
-		case '/about':
-			res.writeHead(200, { 'Content-Type': 'text/plain'});
-			res.end('About');
-			break;
-		default:
-			res.writeHead(404, { 'Content-Type': 'text/plain'});
-			res.end('Not Found');
-			break;
-	}
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-}).listen(3000);
+app.set('port', process.env.PORT || 3000);
 
-console.log('Server started on localhost:3000; press Ctrl-C to terminate...');
+app.use(express.static(__dirname + '/public')); 
+
+//mocha tests 
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.render('home'); 
+});
+
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+}); 
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.'); 
+}); 
